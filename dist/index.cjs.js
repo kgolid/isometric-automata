20ce0bbbb989975ce9e5c39e415a1431975f0352
Kjetil Midtgarden Golid <kjetil.golid@gmail.com>
2020-01-05 17:51:54 +0100

    fix runtime error

--- dist/index.cjs.js
+++ dist/index.cjs.js
@@ -214,8 +214,6 @@ function get_combine_function(n) {
   return (a, b) => combination_tables[n - 2][b][a];
 }
 
-const combination_tables = [table2, table3, table4, table5, table6];
-
 const table2 = [
   [1, 0],
   [0, 1]
@@ -250,6 +248,8 @@ const table6 = [
   [2, 5, 5, 1, 3, 0],
   [1, 3, 0, 0, 2, 4]
 ];
+
+const combination_tables = [table2, table3, table4, table5, table6];
 
 let rng;
 let grid;
